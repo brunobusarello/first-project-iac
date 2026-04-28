511ea63f98165025a64ccc6fd8d0342a48571f74
Bruno Gustavo Busarello <150341182+brunobusarello@users.noreply.github.com>
2026-04-27 21:37:12 -0300

    First commit

---
+++ main.tf
@@ -0,0 +1,9 @@
+resource "aws_s3_bucket" "my-bucket" {
+  bucket = "brunobusarello-first-bucket-${terraform.workspace}"
+
+  tags = {
+    Name = "Primeiro bucket"
+    Iac = true
+    context = "${terraform.workspace}"
+  }
+}
